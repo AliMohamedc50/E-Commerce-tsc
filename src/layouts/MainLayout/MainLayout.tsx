1aa190483e27c2e655c7f4b9738de2ae0e61bb25
Ali Castillo <alicastilo1097@gmail.com>
2024-07-15 09:30:48 +0300

    first commit

---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+function MainLayout() {
+  return (
+    <Container>
+      MainLayout
+    </Container>
+  )
+}
+
+export default MainLayout
---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+
+import ReactDOM from 'react-dom/client'
+import MainLayout from './layouts/MainLayout/MainLayout'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+<MainLayout />
+)
