6070b2ef448b86ab0ac7d4a103d97b7aa4469c2e
Ali Castillo <alicastilo1097@gmail.com>
2024-07-16 10:31:13 +0300

    test

---
+++ src/Components/common/Header/Header.tsx
@@ -0,0 +1,34 @@
+import style from "./Styles.module.css" ;
+
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
+import HeaderBasket from "../../eCommerce/HeaderBasket/HeaderBasket";
+const { headerContainer, headerLogo } = style;
+
+function Header() {
+  return (
+    <header>
+      <div className={headerContainer}>
+        <h1 className={headerLogo}><span>Our</span><Badge>Ecom</Badge></h1>
+        <HeaderBasket />
+      </div>
+        <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+      <Container >
+       
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            <Nav >
+                <Nav.Link href="#home">Login</Nav.Link>
+                <Nav.Link href="#link">Register</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
+  )
+}
+
+export default Header
