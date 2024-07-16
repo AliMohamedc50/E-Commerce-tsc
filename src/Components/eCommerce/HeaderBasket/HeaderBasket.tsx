dd2e02909ca8762c6e9c69362f8b0bf1efe73ec1
Ali Castillo <alicastilo1097@gmail.com>
2024-07-16 10:07:01 +0300

    install: vite-plugin-svgr so can i import svg like component

---
+++ src/Components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -0,0 +1,14 @@
+import  Logo from "../../../assets/svg/cart.svg?react"
+import style from "./styles.module.css"
+
+const {basketContainer, basketQuantity} = style;
+const HeaderBasket = () => {
+  return (
+    <div className={basketContainer}>
+      <Logo />
+      <div className={basketQuantity}>0</div>
+    </div>
+  )
+}
+
+export default HeaderBasket
